cc842fb2ce248431444bb20bf89ef6de0632475c
Xavier Escudero <xavier.escudero_sabadell@roche.com>
2020-04-22 00:30:08 +0200

    Added oscommerce PHP code

---
+++ src/public_html/includes/configure.php
@@ -0,0 +1,31 @@
+<?php
+  define('HTTP_SERVER', 'http://infallible-aggravat.000webhostapp.com');
+  define('HTTPS_SERVER', 'http://infallible-aggravat.000webhostapp.com');
+  define('ENABLE_SSL', false);
+  define('HTTP_COOKIE_DOMAIN', '');
+  define('HTTPS_COOKIE_DOMAIN', '');
+  define('HTTP_COOKIE_PATH', '/');
+  define('HTTPS_COOKIE_PATH', '/');
+  define('DIR_WS_HTTP_CATALOG', '/');
+  define('DIR_WS_HTTPS_CATALOG', '/');
+  define('DIR_WS_IMAGES', 'images/');
+  define('DIR_WS_ICONS', DIR_WS_IMAGES . 'icons/');
+  define('DIR_WS_INCLUDES', 'includes/');
+  define('DIR_WS_FUNCTIONS', DIR_WS_INCLUDES . 'functions/');
+  define('DIR_WS_CLASSES', DIR_WS_INCLUDES . 'classes/');
+  define('DIR_WS_MODULES', DIR_WS_INCLUDES . 'modules/');
+  define('DIR_WS_LANGUAGES', DIR_WS_INCLUDES . 'languages/');
+
+  define('DIR_WS_DOWNLOAD_PUBLIC', 'pub/');
+  define('DIR_FS_CATALOG', '/storage/ssd3/252/13333252/public_html/');
+  define('DIR_FS_DOWNLOAD', DIR_FS_CATALOG . 'download/');
+  define('DIR_FS_DOWNLOAD_PUBLIC', DIR_FS_CATALOG . 'pub/');
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'id13333252_oscommerc');
+  define('DB_SERVER_PASSWORD', ']@k3-K6LBA-iO9)Y');
+  define('DB_DATABASE', 'id13333252_oscommerce');
+  define('USE_PCONNECT', 'false');
+  define('STORE_SESSIONS', 'mysql');
+  define('CFG_TIME_ZONE', 'Europe/Madrid');
+?>
